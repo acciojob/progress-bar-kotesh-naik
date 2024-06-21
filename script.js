d8346ed07d9ea82b15e05cb732fc428a3e9ecb0f
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-21 12:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+
+const circles = document.querySelectorAll(".circle");
+const lines =document.querySelectorAll(".line");
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+let currentActive = 1;
+function handleNextButton(){
+	currentActive++;
+	if(currentActive > circles.length){
+		currentActive =circles.length;
+	}
+	updateUI();
+}
+function handlePrevButton(){
+	currentActive--;
+	if(currentActive < 1){
+		currentActive = 1;
+	}
+	updateUI();
+}
+function updateUI(){
+	circles.forEach((circle,idx) => {
+		idx < currentActive ? 
+			circle.classList.add("active"):
+			circle.classList.remove("active");
+	})
+	lines.forEach((line,idx)=>{
+		idx < currentActive ?
+			line.classList.add("active"):
+			line.classList.remove("active");
+	})
+	prevButton.disabled = currentActive === 1;
+	nextButton.disbled = currentActive === circle.length;
+}
